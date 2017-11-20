b03d6747b5f5ad490e15ecb9a80704e59afdc96f
Eli Rhyne <33841290+Eli-Rhyne@users.noreply.github.com>
2017-11-20 13:45:37 -0500

    Update basic.js

--- modules/basic.js
+++ modules/basic.js
@@ -1,1 +1,24 @@
-//this is a test file
+
+
+
+export function apply(msg, args){
+  if(args.length < 2) return msg.channel.sendMessage("You did not define an argument. Usage: `?apply [role] [reason]`"); //check for role, reason
+    else {
+      msg.delete().catch(console.error); //delete message from chat
+      let reasonFor = args.slice(1).join(" "); //set reason to arguments
+      msg.guild.channels.find("name", "mod_log").sendEmbed({
+        color: 7013119,
+        author: {
+          name: msg.author.username,
+          icon_url: msg.author.avatarURL
+        },
+        title: 'Role Application',
+        description: `${msg.author} has applied for the **${args[0]}** Role`,
+        fields: [{
+          name:'Reason',
+          value: reasonFor
+        }],
+        timestamp: new Date()
+      }); //send embedded message
+    }
+}
