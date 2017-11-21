d6b851bd230875a1a26e85b3474b3629cf019499
Eli Rhyne <33841290+Eli-Rhyne@users.noreply.github.com>
2017-11-20 22:39:28 -0500

    Added roles and apply, all others missing...

--- modules/basic.js
+++ modules/basic.js
@@ -22,3 +22,43 @@ export function apply(msg, args){
       }); //send embedded message
     }
 }
+ export function roles(msg, args){
+ msg.channel.sendEmbed({
+      color: 10826739,
+      author: {
+        name: msg.guild.name,
+        icon_url: msg.guild.iconURL
+      },
+      title: 'Roles',
+      description: "Below is a list of roles and a short description describing them.",
+      fields: [{
+        name:'Admin (Primary Role)',
+        value:'administrator role, they are the owners and rulers of the server'
+      },{
+        name:'Mod (Primary Role)',
+        value:'moderator role, they are here to help and have some power over the server and those in it'
+      },{
+        name:'Bot (Primary Role)',
+        value:'role given to all bots that join/are invited to the server, allowing a clear separation from members'
+      },{
+        name:'Senior Member (Primary Role)',
+        value:'role given to members who are actively participating in the server and have been so for an extended period of time, with a little more effort youll be a staff member in no time'
+      },{
+        name:'Member (Primary Role)',
+        value:'basic role, but shows you are a regular on the server and community'
+      },{
+        name:'Staff (Secondary Role)',
+        value:'pre-moderator role, they are here to help those new to the server and are given little power over the server to show that they are trustworthy'
+      },{
+        name:'Streamer (Secondary Role)',
+        value:'streamer role, given to members who would like to stream on the channel with the highest quality of sound possible and permission to manage the members in that channel'
+      },{
+        name:'Streaming (Secondary Role)',
+        value:'streamer friend role, given to individuals that are allowed to participate in streams giving them voice in the recording chats'
+      },{
+        name:'Bot Chief (Secondary Role)',
+        value:'bot management role, allows you to use certain commands on the bots, like skipping songs without a vote or controlling the volume'
+      }],
+      timestamp: new Date()
+    }).catch(console.error);
+ }
