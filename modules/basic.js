a59bbc7a24c5791d1e1e98b76aa3211df70457c1
Ikaika <iDiffusion@users.noreply.github.com>
2017-11-27 11:30:54 -0500

    Update basic.js
    Thinking about creating a JSON file to hold the descriptions of all the roles and allow users to add more role descriptions as time goes on.

--- modules/basic.js
+++ modules/basic.js
@@ -1,28 +1,33 @@
 
-
-
 export function apply(msg, args){
-  if(args.length < 2) return msg.channel.sendMessage("You did not define an argument. Usage: `?apply [role] [reason]`"); //check for role, reason
-    else {
-      msg.delete().catch(console.error); //delete message from chat
-      let reasonFor = args.slice(1).join(" "); //set reason to arguments
-      msg.guild.channels.find("name", "mod_log").sendEmbed({
-        color: 7013119,
-        author: {
-          name: msg.author.username,
-          icon_url: msg.author.avatarURL
-        },
-        title: 'Role Application',
-        description: `${msg.author} has applied for the **${args[0]}** Role`,
-        fields: [{
-          name:'Reason',
-          value: reasonFor
-        }],
-        timestamp: new Date()
-      }); //send embedded message
-    }
+  let reasonFor = args.slice(1).join(" "); //set reason to arguments
+  let modlog = msg.guild.channels.find("name", "mod_log")
+  msg.delete().catch(console.error); //delete message from chat
+  if(args.length < 2) {
+    return msg.channel.sendMessage("You did not define an argument. Usage: `?apply [role] [reason]`"); //check for role, reason
+  }
+  else if(!modlog) {
+    msg.channel.sendmessage("#mod_log does not exist please ask server owner to add this channel before using this function `?apply`");
+   }
+  else {
+    modlog.sendEmbed({
+      color: 7013119,
+      author: {
+        name: msg.author.username,
+        icon_url: msg.author.avatarURL
+      },
+      title: 'Role Application',
+      description: `${msg.author} has applied for the **${args[0]}** Role`,
+      fields: [{
+        name:'Reason',
+        value: reasonFor
+      }],
+      timestamp: new Date()
+    }); //send embedded message
+  }
 }
- export function roles(msg, args){
+
+ export function roles(msg){
  msg.channel.sendEmbed({
       color: 10826739,
       author: {
