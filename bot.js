848021f4c12e357f5ee0c884cd69296b75e27cb9
Ikaika <iDiffusion@users.noreply.github.com>
2019-09-11 21:40:35 -0400

    Updates as of 9/11/2019

--- bot.js
+++ bot.js
@@ -1,314 +1,173 @@
 "use strict";
 
-const Discord = require("discord.js"); //import the discord.js module
-const bot = new Discord.Client(); //create an instance of a Discord Client, and call it bot
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-// const CleverBot = require("cleverbot-node");
-// const cleverBot = new CleverBot();
-
-const auth = require("./auth.json"); // import the authorzation file
-const config = require("./config/config.json"); // import the config file
+const auth = require("./auth.json");
+const config = require("./config.json");
+const cmds = require("./commands.json");
 
 const utils = require("./modules/utility.js");
 const fun = require("./modules/fun.js");
 const basic = require("./modules/basic.js");
-const setup = require("./modules/config.js");
+const reserved = require("./modules/reserved.js");
 const manage = require("./modules/management.js");
 
 var usersRemoved = [];
-const editingCode = true;
 
-bot.login(auth.token);//Login to bot
-bot.on('error', e => { console.error(e); }); //log error to console
-bot.on('warn', e => { console.warn(e); }); //log warning to console
-//bot.on('debug', e => { console.debug(e); }); //log debugs to console
-bot.on('disconnect', () => { console.log('Celestial has left the building!'); }); //log bot disconnected to console
-bot.on('reconneting', () => { console.log('Attempting to find Celestial.'); }); //log bot reconneting to console
-bot.on('ready', () => { //Display ready when bot is active
+bot.login(auth.token);
+bot.on('error', e => {
+  console.error(e);
+});
+bot.on('warn', e => {
+  console.warn(e);
+});
+bot.on('disconnect', () => {
+  console.log('Celestial has left the building!');
+});
+bot.on('reconneting', () => {
+  console.log('Attempting to find Celestial.');
+});
+bot.on('resume', () => {
+  console.log('Celestial has been located.');
+});
+
+bot.on('ready', () => {
   console.log('Celestial is ready to serve!');
-  if(editingCode) {
-    bot.user.setGame('with code');
+  if (auth.debug) {
+    bot.user.setActivity('with code');
   } else {
-    let randNumber = Math.floor(Math.random() * auth.messages.length);
-    bot.user.setGame(auth.messages[randNumber]).replace('#users', bot.users.array().length);
+    let num = Math.floor(Math.random() * auth.messages.length);
+    let message = auth.messages[num].replace(/#users/gi, bot.users.array().length);
+    bot.user.setActivity(message);
   }
 });
 
-bot.on("message", msg => { //Scan messages in text channels
-  var PREFIX = config.prefix;
-  if(msg.channel.type != 'text') return; //If messages is in text channel continue
-  else if(msg.content.startsWith(PREFIX + " ")) return; //If starts with prefix then space return
-  else if(!msg.content.startsWith(PREFIX)) return; //If has prefix continue
-  else if(msg.author.bot) return; //If not a bot continue
+function getCommand(cmdName) {
+  let commands = cmds.cmds.filter(cmd => {
+    return cmd.alias.filter(p => p.trim().toLowerCase() == cmdName.trim().toLowerCase()).length > 0;
+  });
+  return commands[0];
+}
 
-  let cmd = msg.content.substr(1).split(" ")[0];
-
-  switch(cmd){
-    //text chat
-    case 'apply':
-    case 'app':
-    basic.apply(PREFIX, msg);
-    break;
-
-    //text chat
-    case 'ban':
-    usersRemoved = manage.banMembers(PREFIX, msg, bot, usersRemoved);
-    break;
-
-    //text chat
-    case 'choose':
-    case 'pick':
-    fun.choose(PREFIX, msg);
-    break;
-
-    //text chat
-    case 'coin':
-    case 'flip':
-    case 'coinflip':
-    case 'cointoss':
-    fun.coinFlip(PREFIX, msg);
-    break;
-
-    //any chat
-    case 'commands':
-    case 'cmds':
-    utils.commands(PREFIX, msg);
-    break;
-
-    //text chat
-    case 'giveaway':
-    basic.giveaway(PREFIX, msg);
-    break;
-
-    //any chat
-    case 'help':
-    case 'h':
-    utils.help(PREFIX, msg);
-    break;
-
-    //any chat
-    case 'info':
-    case 'i':
-    case '411':
-    utils.information(PREFIX, msg);
-    break;
-
-    //any chat
-    case 'invite':
-    case 'inv':
-    utils.invite(PREFIX, msg, bot, config, auth);
-    break;
-
-    //text chat
-    case 'kick':
-    usersRemoved = manage.kickMembers(PREFIX, msg, bot, usersRemoved);
-    break;
-
-    //text chat
-    case 'letsplay':
-    case 'lp':
-    fun.letsplay(PREFIX, msg);
-    break;
-
-    //text chat
-    case 'move':
-    case 'm':
-    manage.moveMembers(PREFIX, msg, bot);
-    break;
-
-    //any chat
-    case 'ping':
-    utils.ping(PREFIX, msg, bot);
-    break;
-
-    //text chat (my guilds only)
-    case 'prisolis':
-    fun.prisolis(PREFIX, msg, bot, config);
-    break;
-
-    //text chat
-    case 'prune':
-    manage.prune(PREIFX, msg, bot);
-    break;
-
-    //text chat
-    case 'purge':
-    utils.purge(PREFIX, msg, bot);
-    break;
-
-    //text chat
-    case 'reverse':
-    fun.reverseMessage(PREFIX, msg);
-    break;
-
-    //text chat
-    case 'roll':
-    case 'rolldice':
-    case 'dice':
-    fun.rolldice(PREFIX, msg);
-    break;
-
-    // text chat
-    case "rps":
-    fun.rps(PREFIX, msg, bot);
-    break;
-
-    //text chat
-    case 'say':
-    case 'speak':
-    utils.say(PREFIX, msg);
-    break;
-
-    //any chat
-    case 'set':
-    setup.setBot(PREFIX, msg);
-    break;
-
-    //text chat
-    case 'softban':
-    usersRemoved = manage.softBanMembers(PREFIX, msg, bot);
-    break;
-
-    //any chat
-    case 'status':
-    setup.statusBot(PREFIX, msg);
-    break;
-
-    //any chat
-    case 'suggestion':
-    utils.makeSuggestion(PREFIX, msg, bot);
-    break;
-
-    //text chat
-    case 'tabletop':
-    case 'tt':
-    fun.tabletop(PREFIX, msg);
-    break;
-
-    //text chat
-    case 'warn':
-    manage.warnMembers(PREFIX, msg);
-    break;
-  }
-});
+function setbase(msg, PREFIX, args, cmd){
+  this.msg = msg;
+  this.PREFIX = PREFIX;
+  this.args = args;
+  this.cmd = cmd;
+}
 
 bot.on("message", msg => {
-  var PREFIX = auth.prefix;
-  if(msg.channel.type != 'dm') return; //If messages is in text channel continue
-  else if(msg.author.bot) return; //If not a bot continue
+  // if (auth.debug) {
+  //   console.log(msg);
+  // }
 
-  var command = [];
+  if (msg.author.bot) return;
 
-  command.push(msg.content.substr(1).split(/ +/g)[0]);
-  command.push(msg.content.split(/ +/g)[0]);
+  let PREFIX = config.channels.filter(g => g.guild_id == msg.guild.id)[0];
+  if (PREFIX == undefined) {
+    PREFIX = auth.default.prefix;
+  } else {
+    PREFIX = PREFIX.prefix;
+  }
 
-  command.map(cmd => {
-    switch(cmd){
-      //dm chat
-      case 'clean':
-      utils.cleanMessages(PREFIX, msg, bot);
-      break;
+  let args = msg.content.trim().replace(/  +/g, ' ').split(' ');
+  if (!args[0].startsWith(PREFIX)) return;
+  if (args[0].startsWith(PREFIX + ' ')) return;
 
-      //any chat
-      case 'commands':
-      case 'cmds':
-      utils.commands(PREFIX, msg);
-      break;
+  let cmd = getCommand(args[0].toLowerCase().substr(1));
+  if (!cmd) return;
 
-      //dm chat
-      case 'eval':
-      case 'e':
-      setup.evalcmd(PREFIX, msg, bot);
-      break;
+  let reqperm = ['SEND_MESSAGES', 'MANAGE_MESSAGES', 'MANAGE_CHANNELS'].filter(p => !msg.guild.me.hasPermission(p));
+  if (reqperm.length != 0) {
+    return msg.guild.owner.send(`Please give ${msg.guild.me.user} the following permissions: \`${reqperm}\`. In the following server: **${msg.guild}**.`).catch(console.error);
+  }
 
-      //any chat
-      case 'help':
-      case 'h':
-      utils.help(PREFIX, msg);
-      break;
+  if (!msg.guild.channels.find(x => x.name == "mod_log")) {
+    msg.guild.createChannel('mod_log', {
+        type: 'text',
+        permissionOverwrites: [{
+          id: msg.guild.id,
+          deny: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+          allow: []
+        }, {
+          id: msg.guild.me.id,
+          deny: [],
+          allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+        }]
+      }).catch(console.error);
+  } else {
+    msg.guild.channels.find(x => x.name == "mod_log").overwritePermissions(msg.guild.me.user, {
+       SEND_MESSAGES: true,
+       VIEW_CHANNEL: true
+    }).catch(console.error);
+  }
 
-      //any chat
-      case 'info':
-      case 'i':
-      case '411':
-      utils.information(PREFIX, msg, bot);
-      break;
+  if (cmd.delete == 0) msg.delete().catch(console.error);
 
-      //any chat
-      case 'invite':
-      case 'inv':
-      utils.invite(PREFIX, msg, bot, config, auth);
-      break;
+  if (!cmd.enable) {
+    return msg.channel.send(`\"${cmd.name}\" command is current unavailable, but should be up and running shortly. Please try again later.`);
+  }
 
-      //any chat
-      case 'ping':
-      utils.ping(PREFIX, msg, bot);
-      break;
-
-      //any chat
-      case 'set':
-      setup.setBot(PREFIX, msg);
-      break;
-
-      //any chat
-      case 'status':
-      setup.statusBot(PREFIX, msg);
-      break;
-
-      //any chat
-      case 'suggestion':
-      utils.makeSuggestion(PREFIX, msg, bot, config);
-      break;
+  if (cmd.category != 'Reserved') {
+    let reqperm = cmd.permission.filter(p => !msg.guild.me.hasPermission(p));
+    if (reqperm != 0) {
+      msg.guild.channels.find(x => x.name == "mod_log").send(`Please give ${msg.guild.me.user} the following permissions: \`${reqperm.join(", ")}\`. In order to run the following command: **${cmd.name}**.`).catch(console.error);
+      return msg.channel.send(`Im sorry to inform you but the bot is missing the required permissions needed to run this comamnd: \`${cmd.name}\`.`);
     }
-  });
+  }
+
+  if (cmd.category == 'Reserved');
+  else if (msg.author.id == 170685476787847168);
+  else if (cmd.permission.filter(p => msg.guild.member(msg.author).hasPermission(p)).length == cmd.permission.length);
+  else {
+    return msg.channel.send(`Im sorry to inform you but you are missing one or more of the requried permissions needed to run this command: \`${cmd.name}\`.`);
+  }
+
+  var base = setbase(msg, PREFIX, args, cmd);
+
+  //TODO go to command category to preform command
+
 });
 
-bot.on("guildMemberAdd", mem => { //Member joins guils
-  let guild =  mem.guild;
-  manage.memberAdded(mem, guild);
+bot.on("guildMemberAdd", mem => {
+  let guild = mem.guild;
+  manage.memberAdded(mem, guild, config);
 });
 
-bot.on("guildMemberRemove", mem => { //Member leaves/kicked
-  let guild =  mem.guild;
+bot.on("guildMemberRemove", mem => {
+  let guild = mem.guild;
   manage.memberRemoved(mem, guild, usersRemoved);
 });
 
-bot.on("guildBanAdd", (guild, mem) => { //Member Banned
+bot.on("guildBanAdd", (guild, mem) => {
   manage.memberBanned(mem, guild, usersRemoved);
 });
 
-/*
-bot.on("guildBanRemove", (guild, mem) => { //Member Unbanned
-  memberUnbanned(mem, guild);
+bot.on("guildBanRemove", (guild, mem) => {
+  mamange.memberUnbanned(mem, guild, usersRemoved);
 });
-*/
 
-bot.on("guildCreate", guild => { //Client joins a new guild
+bot.on("guildCreate", guild => {
   console.log(`New guild added: ${guild}.`);
 });
 
-bot.on("guildDelete", guild => { //CLient leaves guild or guild is deleted
+bot.on("guildDelete", guild => {
   console.log(`Old guild left/deleted: ${guild}.`);
 });
 
-bot.on("guildMembersChunk", (mem, guild) => { //Members joins guild
-  mem.map(m => memberAdded(m, guild));
+bot.on("guildMembersChunk", (mem, guild) => {
+  mem.map(m => manage.memberAdded(m, guild, config));
 });
 
-bot.on("guildMemberUpdate", (oldMem, newMem) => { //Member updates info
+bot.on("guildMemberUpdate", (oldMem, newMem) => {
   manage.memberUpdated(oldMem, newMem);
 });
 
-bot.on("guildUpdate", (oldGuild, newGuild) => { //Guild updates info
+bot.on("guildUpdate", (oldGuild, newGuild) => {
   console.log(`${oldGuild.name} has changed the name to ${newGuild.name}.`);
 });
 
-/*
-bot.on("presenceUpdate", (oldMem, newMem) => { //Member updates presence
-  console.log(`${oldMem.username}`)
-});
-*/
-
-bot.on("userUpdate", (oldMem, newMem) => { //Member updats info
+bot.on("userUpdate", (oldMem, newMem) => {
   manage.memberUpdated(oldMem, newMem);
 });
