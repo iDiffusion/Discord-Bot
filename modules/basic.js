a4050de3fb66f886ea592c7eb4d9f92e011f3a8b
Ikaika <iDiffusion@users.noreply.github.com>
2019-11-13 04:27:06 -0500

    Update modules

--- modules/basic.js
+++ modules/basic.js
@@ -1,32 +1,162 @@
-exports.apply = (PREFIX, msg) => {
-  msg.delete().catch(console.error); //delete message from chat
-  let args = msg.content.trim().split(/ +/g).slice(1);
-  let modlog = msg.guild.channels.find("name", "mod_log");
-  let reasonFor = args.slice(1).join(" ");
-  if(args.length < 2) {
-    return msg.channel.sendMessage(`You did not define an argument. Usage: \`${PREFIX}apply [role] [reason]\``); //check for role, reason
+exports.apply = (base) => {
+  let reasonFor = base.args.slice(2).join(" ");
+  if (base.args.length <= 2) {
+    return `You did not define an argument. Usage: \`${base.PREFIX + base.cmd.format}\``;
+  } else if (base.msg.guild.roles.find(r => r.name == args[1].toLowerCase()).length != 0) {
+    return `Im sorry to inform you but you must have at least one role in order to run this command: \`${base.cmd.name}\`.`;
   }
-  else if(!modlog) {
-    msg.channel.sendMessage("Unable to find **#mod_log** text channel, please consult an admin or server owner.");
-   }
-  else {
-    modlog.sendEmbed({
+  base.msg.guild.channels.find(c => c.name == "mod_log");.send({
+    embed: {
       color: 7013119,
       author: {
-        name: msg.author.username,
-        icon_url: msg.author.avatarURL
+        name: base.msg.author.username,
+        icon_url: base.msg.author.avatarURL
       },
       title: 'Role Application',
-      description: `${msg.author} has applied for the **${args[0]}** Role`,
+      description: `${base.msg.author} has applied for the **${base.args[1]}** Role`,
       fields: [{
-        name:'Reason',
+        name: 'Reason',
         value: reasonFor
       }],
       timestamp: new Date()
-    }); //send embedded message
+    }
+  }).catch(console.error);
+};
+
+exports.broadcast = (base) => {
+
+};
+
+exports.clean = (base) => {
+  base.msg.channel.fetchMessages().then(msgs => {
+    let msg_array = message.array().filter(message => message.id == bot.id);
+    msg_array.map(m => m.delete().catch(console.error));
+  });
+};
+
+exports.giveaway = (base) => {
+  //TODO change to message.createReactionCollector(filter, [option]);
+  let message = base.msg.channel.fetchMessage(args[0]).catch(console.error);
+  if (!message) {
+    return `You did not define an argument. Usage: \`${base.PREFIX + base.cmd.format}\``;
+  }
+  let users = [];
+  let array = [];
+  let reactions = message.reactions.array().map(r => r.users.array().map(u => {
+    users.push(u);
+    array.push(u.id);
+  }));
+  array = Array.from(new Set(array));
+  if(base.debug){
+    console.log("This is the messsage: \n " + message.content);
+    console.log(users);
+  }
+  return `${users.find(u => u.id == array.random())} you have won!`;
+};
+
+exports.help = (base) => {
+  let cmd = undefined;
+  if (base.args.length == 1 || (cmd = base.cmds.find(c => c.name == args[1].toLowerCase()))) {
+    let cmds = base.cmds.filter(c => base.msg.member.hasPermissions(c.permision));
+    let array = [];
+    cmds.map(c => array.push(c.name));
+    return array.join(", ");
+  }
+  msg.send({
+    embed: {
+      color: 3447003,
+      title: `Command: ${cmd.name}`,
+      description:
+      `Alias: ${cmd.alias.join(", ")}` +
+      `Description: ${cmd.description}` +
+      `Category: ${cmd.category}\n` +
+      `Permission: ${cmd.permission.join(", ")}\n` +
+      `Format: ${cmd.format}\n` +
+      `Example: ${cmd.example}\n`,
+      thumbnail: {url: guild.iconURL},
+      timestamp: new Date()
+    }
+  });
+};
+
+exports.info = (base) => {
+  if (args.length == 1) {
+    return `You did not define an argument. Usage: \`${base.PREFIX + base.cmd.format}\``;
+  } else if (args[1].toString().toLowerCase() == 'server') {
+    let guild = msg.channel.guild;
+    let botCount = guild.members.filter(mem => mem.user.bot).length;
+    if (isNaN(botCount)) botCount = 1;
+    msg.channel.sendEmbed({
+      color: 262088,
+      title: `Server info for ${msg.guild.name}`,
+      description: `Guild Id: ${guild.id}\n` +
+        `Created: ${new Date(guild.createdAt).toUTCString()}\n` +
+        `Owner: ${guild.owner.displayName}\n` +
+        `Members: ${guild.members.size - botCount}\tBots: ${botCount}\n` +
+        `Icon URL: ${guild.iconURL}`,
+      thumbnail: {
+        url: guild.iconURL
+      },
+      timestamp: new Date()
+    });
+  } else {
+    try {
+      let user = args[1].toString().toLowerCase() == 'bot' ? bot.user : msg.mentions.users.first();
+      msg.channel.sendEmbed({
+        color: 3447003,
+        title: `User info for ${user.username}`,
+        description: `Username: ${user.username}   Nickname: ${msg.guild.member(user).nickname}\n` +
+          `User ID: ${user.id}\n` +
+          `Discriminator: ${user.discriminator}\n` +
+          `Created: ${new Date(user.createdAt).toUTCString()}\n` +
+          `Joined: ${new Data(msg.guild.member(user).joinedTimestamp).toUTCString()}\n` +
+          `Avatar URL: ${user.avatarURL}`,
+        thumbnail: {
+          url: user.avatarURL
+        },
+        timestamp: new Date()
+      });
+    } catch (e) {
+      msg.reply("Please `@mention` a user to see that users information. " + args[0] + " is not a mention.");
+      deleteAfterTime(msg, 3000, 2);
+    }
   }
 }
 
-exports.giveaway = (PREFIX, msg) => {
-  return msg.channel.sendMessages(`This command is currently unavaiiable, if you wish to know when this command will be implemented feel free to message me.`);
+exports.invite = (base) => {
+  let config = base.config.find(g => g.id == base.msg.guild.id);
+  if (base.args.length != 1) {
+    if (base.args[1].toLowerCase().startsWith("perm")) {
+      let link = config && config.server_link ? config.server_link: base.msg.channel.createInvite({maxAge: 0});
+      return `${link} is a permanent link for new members.`;
+    } else if (base.args[1].toLowerCase().startsWith("temp")) {
+      let link = config && config.server_link ? config.server_link: base.msg.channel.createInvite({temporary: true, maxAge: 0});
+      return `${link} is a temporary link for visiting members.`;
+    } else if (base.args[1].toLowerCase().startsWith("bot")) {
+      return `${auth.bot_link} is a invite limk for me.`;
+    }
+  }
+  return `You did not define an argument. Usage: \`${base.PREFIX + base.cmd.format}\``;
+}
+
+exports.say = (base) => {
+  if(args.length == 1) {
+    return `You did not define an argument. Usage: \`${base.PREFIX + base.cmd.format}\``;
+  } else {
+    msg.channel.send(args.join(" "));
+  }
+}
+
+exports.tabletop = (base) => {
+  let channelName = base.cmd.name;
+  let table = base.msg.guild.channels.find(c => c.name == channelName);
+  if(!table) {
+    msg.guild.createChannel("tabletop", "text", [
+      {'id': base.msg.guild.id, 'type': 'role', 'deny': 805449744, 'allow': 379969},
+      {'id': base.msg.author.id, 'type': 'member', 'deny': 536875024, 'allow': 268954689}]);
+    return `Text channal named #${channelName} has been created. Admins please use \`${base.PREFIX + base.cmd.format}\` to delete the channel after use.`);
+  } else {
+    table.delete().catch(console.error);
+    return `Text channel named #${channelName} has been deleted.`);
+  }
 }
--- modules/requested.js
+++ modules/requested.js
@@ -1,1 +1,30 @@
-//This is just a placeholder
+exports.prisolis = (base) => {
+  let channelName = "Story Time w/ Mr.Z";
+  if (base.msg.guild.id != 212624757818916864) { // limit to one guild
+    return;
+  } else if (!checkBotPerm(base, base.cmd.permission, base.cmd.name)) { // check if bot has permission
+    return;
+  } else if (base.msg.author.id != 222883669377810434 && !checkUserPerm(base, base.cmd.permission, base.cmd.name)) { // check if user has permission
+    return;
+  } else if (!checkCmdChat(base)) { // check if in command channel
+    return;
+  }
+  let storyTime = base.msg.guild.channels.find(c => c.name == channelName);
+  if (!storyTime) {
+    base.msg.guild.createChannel(channelName, "voice", [
+      {'id': base.msg.guild.id, 'type': 'role', 'deny': 838860816, 'allow': 3145729},
+      {'id': '222883669377810434', 'type': 'member', 'deny': 536870928, 'allow': 334495745}
+    ]);
+    sendMessage(base.msg.channel,`Voice channel named \`${channelName}\` has been created. Use \`?prisolis\` to delete the channel after use.`);
+  } else {
+    let mem_array = storyTime.members.array();
+    try {
+      let moveToChannel = base.msg.guild.channels.find("name", "General");
+      mem_array.map(id => base.msg.guild.member(id).setVoiceChannel(moveToChannel).catch(console.error));
+    } catch (e) {
+      //TODO move to first voice channel
+    }
+    storyTime.delete().catch(console.error);
+    sendMessage(base.msg.channel, `Voice channel named \`${channelName}\` has been deleted.`);
+  }
+};
